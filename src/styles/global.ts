dd0ecddaeb2edf590c8113fcaaeb39888ac63df9
OrbiStudioOficial <orbistudio3doficial@gmail.com>
2023-09-25 16:27:14 -0300

    update font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,30 +1,31 @@
 import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
-/* red-hat-display-regular - latin */
-@font-face {
-  font-family: 'Red Hat Display';
-  font-style: normal;
-  font-weight: 400;
-  src: local(''),
-       url('/fonts/red-hat-display-v12-latin-regular.woff2') format('woff2');
-}
-/* red-hat-display-600 - latin */
-@font-face {
-  font-family: 'Red Hat Display';
-  font-style: normal;
-  font-weight: 600;
-  src: local(''),
-       url('/fonts/red-hat-display-v12-latin-600.woff2') format('woff2');
-}
-/* red-hat-display-900 - latin */
-@font-face {
-  font-family: 'Red Hat Display';
-  font-style: normal;
-  font-weight: 900;
-  src: local(''),
-       url('/fonts/red-hat-display-v12-latin-900.woff2') format('woff2');
+/* wix-madefor-display-regular - latin */
+  @font-face {
+    font-display: swap;
+    font-family: 'Wix Madefor Display';
+    font-style: normal;
+    font-weight: 400;
+    src: url('/fonts/wix-madefor-display-v10-latin-regular.woff2') format('woff2');
   }
+  /* wix-madefor-display-600 - latin */
+  @font-face {
+    font-display: swap;
+    font-family: 'Wix Madefor Display';
+    font-style: normal;
+    font-weight: 600;
+    src: url('/fonts/wix-madefor-display-v10-latin-600.woff2') format('woff2');
+  }
+  /* wix-madefor-display-800 - latin */
+  @font-face {
+    font-display: swap;
+    font-family: 'Wix Madefor Display';
+    font-style: normal;
+    font-weight: 800;
+    src: url('/fonts/wix-madefor-display-v10-latin-800.woff2') format('woff2');
+  }
+
   * {
     margin: 0;
     padding: 0;
